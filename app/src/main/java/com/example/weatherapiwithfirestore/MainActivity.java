6a06403c1e2010e1a15c01dd4f59ced1b1b6b2a4
Parag Dangriya <45040382+ParagsDangariya@users.noreply.github.com>
2019-09-19 14:55:51 -0400

    Login and registration done..

--- app/src/main/java/com/example/weatherapiwithfirestore/MainActivity.java
+++ app/src/main/java/com/example/weatherapiwithfirestore/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.weatherapiwithfirestore;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
